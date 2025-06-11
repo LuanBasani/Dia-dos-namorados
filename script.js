b6c563d715bb33d2fdf258daa0b0fd2b71a03bb9
LuanBasani <luanbasani@gmail.com>
2025-06-11 15:48:10 -0300

    Adicionando arquivos do index, css, javascript, imagens e videos

---
+++ script.js
@@ -0,0 +1,5 @@
+// Inicializa a biblioteca de animação AOS (Animate On Scroll)
+AOS.init({
+    duration: 800, // Duração da animação em milissegundos
+    once: true,    // A animação acontece apenas uma vez
+});
